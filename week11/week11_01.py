caad7be2c7b00023d9f8cba225ced14c9d7161fa
shj02 <163235571+shj02@users.noreply.github.com>
2024-11-06 10:23:17 +0900

    class

---
+++ week11/week11_01.py
@@ -0,0 +1,16 @@
+# week11_01.py
+
+class Test: # Test 클래스(자료형) 선언
+    pass
+
+t1 = Test() # 생성자 Test를 호출
+                # 메모리에 Test 타입의 데이터가 생성
+                #       >> Test의 instance
+                # 그 데이터를 t1이 참조하고 있음
+
+i1 = int(1) # 생성자 int를 호출
+                # 메모리에 int타입의 데이터(1)가 생성
+                # 그 데이터를 i1이 참조하고 있음
+
+print(type(t1))
+print(type(i1))
