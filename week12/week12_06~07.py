65d83fd588ea1a5588daad8c2941c9985176c45f
shj02 <163235571+shj02@users.noreply.github.com>
2024-12-03 18:58:33 +0900

    import

--- week12/week12_06~07.py
+++ week12/week12_06~07.py
@@ -1,5 +1,4 @@
 # week12_06.py
-
 PI = 3.141592
 
 class Math:
@@ -13,13 +12,13 @@ def add(a, b):
 
 
 # week12_07.py
-
 import week12_06 as model
-from week12_06 import PI
-from week12_06 import PI as pi
-
 print(model.PI)
+
+from week12_06 import PI
 print(PI)
+
+from week12_06 import PI as pi
 print(pi)
 
 print(model.add(model.PI, 4.4))
